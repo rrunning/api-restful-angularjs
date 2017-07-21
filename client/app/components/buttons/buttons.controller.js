5e7300e1a598767158268a91e90b0f13f7bcad72
Ryan Running <ryanrunning@securitymetrics.com>
2017-07-20 20:48:36 -0600

    adds the arrays for user and default words

--- client/app/components/buttons/buttons.controller.js
+++ client/app/components/buttons/buttons.controller.js
@@ -2,6 +2,9 @@ class ButtonsController {
   constructor() {
     this.name = 'buttons';
   }
+  showStory(story) {
+    // show a specific story
+  }
 }
 
 export default ButtonsController;
--- client/app/components/formFields/formFields.controller.js
+++ client/app/components/formFields/formFields.controller.js
@@ -1,6 +1,18 @@
 class FormFieldsController {
   constructor() {
     this.name = 'formFields';
+    this.userWords = {
+      noun: [],
+      adjective: [],
+      verb: [],
+      adverb: []
+    };
+    this.defaultWords = {
+      noun: ['table', 'shirt', 'airplane', 'shark', 'computer', 'hedgehog', 'telephone', 'can', 'market', 'marker'],
+      verb: ['choose', 'feed', 'fling', 'bite', 'awake', 'grind', 'kneel', 'misspell', 'overthrow', 'ring'],
+      adjective: ['bland', 'deficient', 'motherly', 'passionate', 'rotten', 'threadbare', 'voluminous', 'ample', 'crafty', 'flustered'],
+      adverb: ['deeply', 'famously', 'brutally', 'anywhere', 'miserably', 'neatly', 'openly', 'reproachfully', 'ruthlessely', 'politely']
+    };
   }
 }
 
